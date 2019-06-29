88d736e2444ba743ba8a5e6129f3a2b415096768
liorbey <lior.bey95@gmail.com>
2019-06-29 13:35:22 -0400

    updated API settings from yelp

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business.js';
 
-class BusinessList extends React.Component{
-    render(){
-        return(
-            <div className="BusinessList">
-            {
-                this.props.businesses.map((business) => {
-                     return <Business business={business}/>
-                })
-            }
-            </div>
-        );
+class BusinessList extends React.Component {
+    render() {
+      return (
+        <div className="BusinessList">
+          {
+            this.props.businesses.map(business => {
+              return <Business business={business} key={business.id} />
+            })
+          }
+        </div>
+      );
     }
-};
-
-export default BusinessList;
+  }
+  
+  export default BusinessList;
